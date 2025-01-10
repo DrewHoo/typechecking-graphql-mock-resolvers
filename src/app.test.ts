ed8048c627e063583e27f731d4a1fc77afcbed19
Drew Hoover <drew@greatexpectations.io>
2025-01-10 12:23:50 -0500

    cleanup

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,9 +1,12 @@
 import { expect, test } from 'vitest'
-import { app, GetCardsDocument } from './app'
+import { graphql } from "src/api/graphql";
 import { createMockData } from 'src/api/mock-data-utils/create-mock-data'
+import { app, GetCardsDocument } from './app'
 
 test('app', () => {
-    const data = createMockData(GetCardsDocument, { AbilitiesListItem: () => ({ effect: "paralyzed" }) })
+    const data = createMockData(GetCardsDocument, {
+        Query: () => ({ cards: () => [{abilities: [{ effect: "paralyzed"}]}]})
+    })
     const result = app(data)
     expect(result).toBe("paralyzed")
 })
